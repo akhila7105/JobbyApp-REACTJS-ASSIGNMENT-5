298d2ebbe613a247c8a6b9327d442ba15684ff1e
akhila7105 <akhilakoripelli21@gmail.com>
2023-06-08 07:02:42 +0000

    JobbyApp-REACTJS-ASSIGNMENT-5

---
+++ src/components/Home/index.js
@@ -0,0 +1,39 @@
+import {Link, Redirect} from 'react-router-dom'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Home = props => {
+  const onRedirectToJobs = () => {
+    const {history} = props
+    history.replace('/jobs')
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="home-container">
+        <h1 className="home-heading">
+          Find The Job That <br />
+          Fits Your Life
+        </h1>
+        <p className="home-paragraph">
+          Millions of people are searching for jobs, salary information, company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+        <Link className="retry-button-link" to="/jobs">
+          <button
+            className="homeJobs-button"
+            type="button"
+            onClick={onRedirectToJobs}
+          >
+            Find Jobs
+          </button>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Home
